f23eaeb6d0186973ee3a43a3c0880626f2965a97
Lionel Carreras <lionelcarreras.it@gmail.com>
2025-06-19 08:52:41 -0300

    ticket v1.2

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'          
+STATICFILES_DIRS = [BASE_DIR / 'static']  
 TEMPLATES[0]['DIRS'] = [ BASE_DIR / 'templates' ]
 
 
