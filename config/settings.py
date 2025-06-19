321464d774126828d9a5891731c5c39bfd335c60
Lionel Carreras <lionelcarreras.it@gmail.com>
2025-06-19 09:19:28 -0300

    static

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'          
-STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']    # aquí apuntas a tu carpeta /static
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 TEMPLATES[0]['DIRS'] = [ BASE_DIR / 'templates' ]
 
 
